5473ebfdd4aedefd5a519f39575e81ba2d22c763
Facoleur <facoleur@gmail.com>
2026-01-31 00:23:09 +0100

    feat: add clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Providers } from "@/components/QueryLayout";
 import type { Metadata } from "next";
 import { getLocale, getMessages } from "next-intl/server";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -102,6 +103,15 @@ export default async function RootLayout({
             <Footer />
           </div>
         </Providers>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
       </body>
     </html>
   );
